de7bd4edf2513c3d981ecfb74be21f61815d6497
Poornad1152 <123054076+Poornad1152@users.noreply.github.com>
2023-01-19 13:53:48 +0530

    Update streamlite_app.py
    added a new header

--- streamlite_app.py
+++ streamlite_app.py
@@ -7,3 +7,6 @@ streamlit.text('Omega 3 &Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text("Hard-Boiled Free-Range Egg")
 streamlit.text('Avocado Toast')
+
+
+streamlit.header('Build your own Fruit Smoothie')
